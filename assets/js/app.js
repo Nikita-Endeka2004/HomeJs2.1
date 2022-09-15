f50f3266373464cf891737e5b532db7174d2c00e
Nikita Endea <90311455+Nikita-Endeka2004@users.noreply.github.com>
2022-09-15 14:03:04 +0300

    jsStart

---
+++ assets/js/app.js
@@ -0,0 +1,126 @@
+
+
+
+    const ICAOInput = "KLBG";
+
+let ICAO = [
+{
+    name: "A",
+    foolName: "Alfa"
+},
+{
+    name: "B",
+    foolName: "Alfa"
+},
+{
+    name: "C",
+    foolName: "Alfa"
+},
+{
+    name: "D",
+    foolName: "Alfa"
+},
+{
+    name: "E",
+    foolName: "Alfa"
+},
+{
+    name: "F",
+    foolName: "Alfa"
+},
+{
+    name: "G",
+    foolName: "Alfa"
+},
+{
+    name: "H",
+    foolName: "Alfa"
+},
+{
+    name: "I",
+    foolName: "Alfa"
+},
+{
+    name: "J",
+    foolName: "Alfa"
+},
+{
+    name: "K",
+    foolName: "Alfa"
+},
+{
+    name: "L",
+    foolName: "Alfa"
+},
+{
+    name: "M",
+    foolName: "Alfa"
+},
+{
+    name: "N",
+    foolName: "Alfa"
+},
+{
+    name: "O",
+    foolName: "Alfa"
+},
+{
+    name: "P",
+    foolName: "Alfa"
+},
+{
+    name: "Q",
+    foolName: "Alfa"
+},
+{
+    name: "R",
+    foolName: "Alfa"
+},
+{
+    name: "S",
+    foolName: "Alfa"
+},
+{
+    name: "T",
+    foolName: "Alfa"
+},
+{
+    name: "U",
+    foolName: "Alfa"
+},
+{
+    name: "V",
+    foolName: "Alfa"
+},
+{
+    name: "W",
+    foolName: "Alfa"
+},
+{
+    name: "X",
+    foolName: "Alfa"
+},
+{
+    name: "Y",
+    foolName: "Alfa"
+},
+{
+    name: "Z",
+    foolName: "Alfa"
+}
+   ];
+
+  let str = ICAOInput.split('');
+
+  console.log(str);
+
+  let strOutput;
+
+  strOutput = str.filter((item) => item.name == str);
+
+  for(let i = 0; i < str.length; i++){
+
+  }
+
+  console.log(strOutput);
+
