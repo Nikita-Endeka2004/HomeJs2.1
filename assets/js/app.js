e53df6fcb8ce745554288083ceb7a7c94b490a10
Nikita Endea <90311455+Nikita-Endeka2004@users.noreply.github.com>
2022-09-15 17:21:00 +0300

    js

--- assets/js/app.js
+++ assets/js/app.js
@@ -1,126 +1,45 @@
 
 
 
-    const ICAOInput = "KLBG";
+    let ICAOInput = "KLBG";
 
-let ICAO = [
-{
-    name: "A",
-    foolName: "Alfa"
-},
-{
-    name: "B",
-    foolName: "Alfa"
-},
-{
-    name: "C",
-    foolName: "Alfa"
-},
-{
-    name: "D",
-    foolName: "Alfa"
-},
-{
-    name: "E",
-    foolName: "Alfa"
-},
-{
-    name: "F",
-    foolName: "Alfa"
-},
-{
-    name: "G",
-    foolName: "Alfa"
-},
-{
-    name: "H",
-    foolName: "Alfa"
-},
-{
-    name: "I",
-    foolName: "Alfa"
-},
-{
-    name: "J",
-    foolName: "Alfa"
-},
-{
-    name: "K",
-    foolName: "Alfa"
-},
-{
-    name: "L",
-    foolName: "Alfa"
-},
-{
-    name: "M",
-    foolName: "Alfa"
-},
-{
-    name: "N",
-    foolName: "Alfa"
-},
-{
-    name: "O",
-    foolName: "Alfa"
-},
-{
-    name: "P",
-    foolName: "Alfa"
-},
-{
-    name: "Q",
-    foolName: "Alfa"
-},
-{
-    name: "R",
-    foolName: "Alfa"
-},
-{
-    name: "S",
-    foolName: "Alfa"
-},
-{
-    name: "T",
-    foolName: "Alfa"
-},
-{
-    name: "U",
-    foolName: "Alfa"
-},
-{
-    name: "V",
-    foolName: "Alfa"
-},
-{
-    name: "W",
-    foolName: "Alfa"
-},
-{
-    name: "X",
-    foolName: "Alfa"
-},
-{
-    name: "Y",
-    foolName: "Alfa"
-},
-{
-    name: "Z",
-    foolName: "Alfa"
+let ICAO = {
+
+    a: 'Alfa',
+    b: 'Bravo',
+    c: 'Charlie',
+    d: 'Delta',
+    e: 'Echo',
+    f: 'Foxtrot',
+    g: 'Golf',
+    h: 'Hotel',
+    i: 'India',
+    j: 'Juliett',
+    k: 'Kilo',
+    l: 'Lima',
+    m: 'Mike',
+    n: 'Novermber',
+    o: 'Oscar',
+    p: 'Papa',
+    q: 'Quebec',
+    r: 'Romeo',
+    s: 'Sierra',
+    t: 'Tango',
+    u: 'Uniform',
+    v: 'Victor',
+    w: 'Whiskey',
+    x: 'X-Ray',
+    y: 'Yankee',
+    z: 'Zulu'
+
 }
-   ];
 
-  let str = ICAOInput.split('');
+let newICAO = ICAOInput.toLowerCase();
+
+  let str = newICAO.split('');
 
   console.log(str);
 
-  let strOutput;
+  console.log(ICAO[newICAO]);
 
-  strOutput = str.filter((item) => item.name == str);
-
-  for(let i = 0; i < str.length; i++){
-
-  }
-
-  console.log(strOutput);
 
